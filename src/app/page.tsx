c5813621e5700f20254c0dd97af167a4884680ca
Chanakan Srisarutiporn <theghostarttv@gmail.com>
2025-02-15 10:03:57 +0700

    [head-section] Create header section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,22 @@
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+    <section className="text-white bg-black py-1">
+      <div className="container text-center relative">
+        <p>Sign up and get 20% off to your first order. Sign Up Now</p>
+        <button type="button" className="absolute top-0 right-0">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+          >
+            <path
+              fill="currentColor"
+              d="M6.4 19L5 17.6l5.6-5.6L5 6.4L6.4 5l5.6 5.6L17.6 5L19 6.4L13.4 12l5.6 5.6l-1.4 1.4l-5.6-5.6z"
+            />
+          </svg>
+        </button>
+      </div>
+    </section>
+  );
 }
